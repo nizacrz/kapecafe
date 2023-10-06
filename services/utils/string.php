8619e062e127d2994c6d521e2746f207319eb481
Yunice Nicolle Cruz <cruz.yuniza@gmail.com>
2023-10-06 13:20:36 +0800

    Removed Sanitize strings

--- services/utils/string.php
+++ services/utils/string.php
@@ -10,24 +10,4 @@ class Str
         return $str;
     }
 
-    // Sanitize strings
-    public static function sanitizeString($raw)
-    {
-        return htmlspecialchars(strip_tags($raw));
-    }
-
-    public static function sanitizeEmail($raw)
-    {
-        return filter_var($raw, FILTER_SANITIZE_EMAIL);
-    }
-
-    public static function sanitizeInt($raw)
-    {
-        return filter_var($raw, FILTER_SANITIZE_NUMBER_INT);
-    }
-
-    public static function sanitizeDouble($raw)
-    {
-        return filter_var($raw, FILTER_SANITIZE_NUMBER_FLOAT);
-    }
 }
