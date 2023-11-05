90fc09c85738216b055b7072a49a13fcafcc4741
Yunice Nicolle Cruz <cruz.yuniza@gmail.com>
2023-11-05 17:24:39 +0800

    Update signin

--- signin.php
+++ signin.php
@@ -34,7 +34,7 @@ if (isset($_SESSION['id'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,
-	 initial-scale=1.0">
+     initial-scale=1.0">
     <title>Sign in | kapecafé</title>
     <link rel="icon" href="/assets/logo/kapecafé-logo.png">
     <link rel="stylesheet" href="/assets/styles/account-style.css">
@@ -51,16 +51,25 @@ if (isset($_SESSION['id'])) {
                 <center><span class="subtitle"> Please login to kapecafé to continue. </span> </center>
                 <br>
                 <?php if (isset($_GET['error'])) { ?>
-                    <p class="fail"><?php echo $_GET['error']; ?></p>
+                    <p class="fail">
+                        <?php echo $_GET['error']; ?>
+                    </p>
                 <?php } ?>
 
                 <?php if (isset($_GET['success'])) { ?>
-                    <p class="success"><?php echo $_GET['success']; ?></p>
+                    <p class="success">
+                        <?php echo $_GET['success']; ?>
+                    </p>
                 <?php } ?>
 
 
                 <div class="input-group">
                     <input type="text" required placeholder="username" name="username">
+                </div>
+                <div class="input-group">
+                    <input type="password" id="password" required placeholder="password" name="password">
+                    <i class="fa-regular fa-eye-slash" id="togglePassword"
+                        style="margin-left: -30px; cursor: pointer;"></i>
                 </div>
 
                 <div class="input-group">
