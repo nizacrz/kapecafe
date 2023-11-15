f4a6619c6577616c3cbbcef62b991056ef6f0d36
Aldwinny <aldwin0313@yahoo.com.ph>
2023-11-15 10:58:13 +0800

    a bit of fix here and there

--- admin/logging.php
+++ admin/logging.php
@@ -4,5 +4,5 @@
 # Logging will also be divided into various types
 # 1. Information - I logged on. I logged off.
 # 2. Warning - Oi, m8, something doesnt make sense.
-# 3. Error - thats rough body.
+# 3. Error - thats rough, buddy.
 # 4. Critical - ENNN ENNN EN NN ENNN, UR ACCOUNT NOW LOCKED GRRR
