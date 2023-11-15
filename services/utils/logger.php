a86dc20086bca9c9a7e9daa3bfe2de6f1e2c0bbd
Aldwinny <aldwin0313@yahoo.com.ph>
2023-11-15 10:57:09 +0800

    Added an initial logger class

---
+++ services/utils/logger.php
@@ -0,0 +1,8 @@
+<?php
+
+# 1. Implement the singleton Design principle..
+# 2. Implement the various levels for Logging (INFO, WARN, ERROR, CRITICAL)
+# 3. Propagate usage to all codes that use SSE.
+class Log
+{
+}
