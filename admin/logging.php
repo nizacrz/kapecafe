93461ad13414eeaee4d68b57cbe53f9017218d06
Aldwinny <aldwin0313@yahoo.com.ph>
2023-11-15 10:55:11 +0800

    Added support files for future SSE implementation.

--- admin/logging.php
+++ admin/logging.php
@@ -1,3 +1,8 @@
-<?php 
+<?php
+# SSE powered console which means 1 way and stuff..
 
-// Future File
+# Logging will also be divided into various types
+# 1. Information - I logged on. I logged off.
+# 2. Warning - Oi, m8, something doesnt make sense.
+# 3. Error - thats rough body.
+# 4. Critical - ENNN ENNN EN NN ENNN, UR ACCOUNT NOW LOCKED GRRR
